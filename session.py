a6b8520fc74a2961afdea6a239cf697f381d6764
KrzMa <marut.krzysztof6@gmail.com>
2023-05-29 14:18:34 +0200

    update session.py

--- session.py
+++ session.py
@@ -1,12 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from getpass import getpass
 
-
-password = getpass(input('Your password: '))
 
 engine = create_engine(
-    f'mysql+pymysql://root:{password}@localhost:3306/blog'
+    'mysql+pymysql://root:password@localhost:3306/blog'
 )
 Session = sessionmaker(bind=engine)
 session = Session()
